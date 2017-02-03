efd7eac334f2723fd4649a1862e7460c574b4ad3
shreeharshini <mamidalaharshini@gmail.com>
2017-02-03 17:24:32 +0530

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get 'content_conversation/index'
+
+	get 'manage_access/index'
+
+	get 'manage_user_account/index'
+
+	get 'printhouse_setup/index'
+
+	get 'publisher_setup/index'
+
+	get 'publishers/search'
+
+	get 'reports/index'
+
+	resource :publishers
 end
