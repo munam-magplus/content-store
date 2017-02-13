0651750e7f95445e7301c93bed59cff15e55b823
Kirti <kirtitrikha09@gmail.com>
2017-02-13 17:07:26 +0530

    nested attributes

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -13,8 +13,11 @@ class PublishersController < ApplicationController
   end
 
   def create
-    current_user = User.active.find(session[:user_id]) unless session[:user_id].nil?
     @publisher = Publisher.new(publisher_params)
+    # not able to access current user, so giving demo values to mandatory fields
+    @publisher.CREATED_BY = "current_user_name"
+    @publisher.LASTUPDATED_BY = "current_user_name"
+    @publisher.publisher_contact.PUC_CONTACT_TYPE = "demo"
     #@publisher.build_publisher_contact
     if @publisher.save!
       redirect_to publishers_path
@@ -25,6 +28,6 @@ class PublishersController < ApplicationController
   private 
 
   def publisher_params
-    params.require(:publisher).permit(:PUB_CODE, :PUB_NAME, :PUB_STATUS,:PUB_END_USR_LANGUAGE, :PUB_ADMIN_TOOL_LANGUAGE_ID, :CREATED_BY,:LASTUPDATED_BY, :publisher_contact_attributes =>[:PUC_CONTACT_FNAME, :PUC_CONTACT_LNAME, :PUC_CONTACT_EMAIL, :PUC_CONTACT_TYPE])
+    params.require(:publisher).permit(:PUB_CODE, :PUB_NAME, :PUB_STATUS,:PUB_END_USR_LANGUAGE, :PUB_ADMIN_TOOL_LANGUAGE_ID, :publisher_contact_attributes =>[:PUC_CONTACT_FNAME, :PUC_CONTACT_LNAME, :PUC_CONTACT_EMAIL])
   end
 end
