265db2bc33dcc02a45a1cb863df631f29e516fe6
Kirti <kirtitrikha09@gmail.com>
2017-02-13 16:09:47 +0530

    Publisher create

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -9,8 +9,22 @@ class PublishersController < ApplicationController
   end
   def new
     @publisher = Publisher.new
+    @publisher.publisher_contact = PublisherContact.new
   end
 
   def create
-  end 
+    current_user = User.active.find(session[:user_id]) unless session[:user_id].nil?
+    @publisher = Publisher.new(publisher_params)
+    #@publisher.build_publisher_contact
+    if @publisher.save!
+      redirect_to publishers_path
+    else
+      render 'new'
+    end
+  end
+  private 
+
+  def publisher_params
+    params.require(:publisher).permit(:PUB_CODE, :PUB_NAME, :PUB_STATUS,:PUB_END_USR_LANGUAGE, :PUB_ADMIN_TOOL_LANGUAGE_ID, :CREATED_BY,:LASTUPDATED_BY, :publisher_contact_attributes =>[:PUC_CONTACT_FNAME, :PUC_CONTACT_LNAME, :PUC_CONTACT_EMAIL, :PUC_CONTACT_TYPE])
+  end
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,8 +1,9 @@
 class Publisher < ApplicationRecord
 	self.table_name = "CDP_PUBLISHER"
 	has_one :publisher_contact, :foreign_key => "PUB_PUBLISHER_ID"
+  	accepts_nested_attributes_for  :publisher_contact
+	has_many :retailers
 	def self.search(code,name)
   	 result = where("pub_code LIKE ? AND pub_name LIKE ?", "%#{code}%", "%#{name}")
-	end
-	has_many :retailers
+	end	
 end
