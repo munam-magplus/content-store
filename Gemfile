1c3c59841c5c82d666f6a6e010e5b895881c6ce4
Kirti <kirtitrikha09@gmail.com>
2017-03-03 16:06:27 +0530

    Exception handling

--- Gemfile
+++ Gemfile
@@ -21,7 +21,10 @@ gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'haml'
+# To use haml in views
 # Use jquery as the JavaScript library
+gem 'exception_handler'
+
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module ContentStore
   class Application < Rails::Application
+  	config.exception_handler = {
+       dev: true,
+       db:     false, #-> defaults to :errors if true, else use :table_name
+       email:  false, #-> need to integrate
+       layouts: {
+         '404' => 'application',
+         '400' => 'application',
+         '500' => 'application'
+        },
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
