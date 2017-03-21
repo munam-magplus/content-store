9f0072a721a1e1524f22351185607101b3bcf59c
Kirti <kirtitrikha09@gmail.com>
2017-03-21 15:41:34 +0530

    converting haml to erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170306044650) do
 
+ActiveRecord::Schema.define(version: 20170306120656) do
   create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "publisher"
     t.string   "email"
