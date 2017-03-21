2a6a7b1bf0a8fb5e4652f5d13e2407be86f39eb9
Kirti <kirtitrikha09@gmail.com>
2017-03-21 15:41:34 +0530

    converting haml to erb

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,11 +3,27 @@ class CampaignsController < ApplicationController
   end
 
   def create
+     @campaign = Campaign.create!(campaign_params)
+     if @campaign.save!
+      # if successfully stored then redirect 
+      redirect_to campaigns
+    else
+      # if not save then render same page
+      render 'new'
+    end
   end
 
   def new
+    @campaign = Campaign.new
   end
 
   def index
+  end
+  def search
+    
+  end
+  private 
+  def campaign_params
+    params.require(:campaign).permit(:pub, :campaign_name)
   end
 end
