dd3b39032dff3b1a0b87d6aa371e1d97fd1beb82
shreeharshini <mamidalaharshini@gmail.com>
2017-03-30 13:08:29 +0530

    institutional admin users

--- app/controllers/institution_account_controller.rb
+++ app/controllers/institution_account_controller.rb
@@ -1,9 +1,12 @@
 class InstitutionAccountController < ApplicationController  
   def index
-    byebug
+    # we render result page only when we find publisher in params.
     if params[:publisher].present?
+      # we perform search operation by slicing the params.
       @res = InstitutionAccount.filter(params.slice(:publisher,:institution_id))
+      # this is the params of the checkbox that is present in the form.
       @chec = params[:ip_address]
+      # we render the result page that have the search result.
       render 'result', locals: { :res => @res, :chec => @chec , :country => @country}
     else
       render 'index'
@@ -18,16 +21,18 @@ class InstitutionAccountController < ApplicationController
   end
 
   def new
-    byebug
     @institution = InstitutionAccount.new
+    @institution.build_institution_acc_billing_address
   end
 
   def create
     byebug
     @institution = InstitutionAccount.new(institution_params)
     #this is added inorder to build institution_acc_billing_address
-    #we use this syntax because the association is has_one
-     @institution.build_institution_acc_billing_address
+    #we use this syntax because the association between institution account
+    # and institution_acc_billing_address is has_one
+    # @institution.build_institution_acc_billing_address
+    # byebug
 
     if @institution.save!
       redirect_to institution_account_index_path
@@ -41,12 +46,12 @@ class InstitutionAccountController < ApplicationController
   private
 
   def institution_params
-    byebug
+    # byebug
     params.require(:institution_account).permit(:publisher,:institution_id, 
         :libary_name,:institution_name,:status,:display_name, 
-        :institution_acc_billing_address_attributes =>[
-        :id, :first_name, :last_name, :phone, :email, :address, 
+        { institution_acc_billing_address_attributes: [
+        :first_name, :last_name, :phone, :email, :address, 
         :address_line2,:address_line3, :city, :state, 
-        :postal_code, :comments]) 
+        :postal_code, :comments, :institution_account_id]} ) 
   end
 end
--- app/controllers/institution_admin_user_controller.rb
+++ app/controllers/institution_admin_user_controller.rb
@@ -5,6 +5,8 @@ class InstitutionAdminUserController < ApplicationController
   end
 
   def search_op
+    # we search admin users by using status,role and first_name.
+    # here, we filter the params and get status,role and first_name only.
     @res = InstitutionAdminUserAcc.filter(params.slice(:status,:role, :first_name))
     render 'result', locals: { :res => @res }
   end
@@ -17,14 +19,20 @@ class InstitutionAdminUserController < ApplicationController
   end
 
   def create
-    byebug
+    # create new institution admin user
     @inst_admin_user = InstitutionAdminUserAcc.new(insti_admin_user_params)
+    # get username and role of the admin user
     @user = params[:institution_admin_user_acc][:user_name]
     @rol = params[:institution_admin_user_acc][:role]
+    # inserting user name with SA prefix only for Institution Administrator
     if @rol == "Institution Administrator"
      @inst_admin_user.user_name = "SA_#{@user}"
     else
+      # inserting user name with PA as prefix and increment the pcount only when
+      # the role is primary institution administrator.
       @inst_admin_user.user_name = "PA#{@pcount}"
+      # store value in primary_count only when role is primary institution administrator.
+      # otherwise primary_count's value is nil.
       @inst_admin_user.primary_count = "#{@pcount}"
     end
     if @inst_admin_user.save!
@@ -35,6 +43,10 @@ class InstitutionAdminUserController < ApplicationController
   end
   
   def set_system_generated_username
+    # the primary Institution Administrator's role is system generated.inorder to achieve 
+    # this,we use primary_count.
+    # if the role is Institution Administrator, the primary_count is nil and if the role 
+    # is primary Institution Administrator,the primary_count value is incremented.
     @pcount = "#{InstitutionAdminUserAcc.where.not(:primary_count => nil).last.primary_count + 1}" 
   end
 
