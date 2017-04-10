9c6172df3e495439529f67a02ac93da80ea7cb16
Kirti <kirtitrikha09@gmail.com>
2017-04-10 16:22:08 +0530

    removed migrations

--- db/migrate/20170330103947_rename_publisher_to_publisher_id_in_promo_codes.rb
+++
@@ -1,8 +0,0 @@
-class RenamePublisherToPublisherIdInPromoCodes < ActiveRecord::Migration[5.0]
-  def self.up
-     rename_column :promo_codes, :publisher, :publisher_id
-     rename_column :promo_codes, :campaign, :campaign_id
-  end
-  def self.down
-  end
-end
--- db/migrate/20170330104040_add_license_for_sale_to_license_group.rb
+++
@@ -1,5 +0,0 @@
-class AddLicenseForSaleTogroupLicense < ActiveRecord::Migration[5.0]
-  def change
-    add_column :license_groups, :license_for_sale, :string
-  end
-end
