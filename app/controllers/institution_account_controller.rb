f424ebde3f318c6f3d8b4ccdfcf3f44cefae41e0
shreeharshini <mamidalaharshini@gmail.com>
2017-04-13 18:37:30 +0530

    manage user account tab

--- app/controllers/institution_account_controller.rb
+++ app/controllers/institution_account_controller.rb
@@ -4,16 +4,14 @@ class InstitutionAccountController < ApplicationController
   end
   
   def search_op
-    byebug
     # we perform search operation by slicing the params.
     @res = InstitutionAccount.filter(params.slice(:publisher, 
-            :status, :libary_name, :institution_name))
-    # this is the params of the checkbox that is present in the form.
+            :status, :libary_name, :institution_name,:country))
     byebug
-    @ins = InstitutionAccBillingAddress.find_by(params[:id])
+    # this is the params of the checkbox that is present in the form.
     @chec = params[:ip_address]
     # we render the result page that have the search result.
-    render 'result', locals: { :res => @res, :chec => @chec , :country => @country} 
+    render 'result', locals: { :res => @res, :chec => @chec} 
   end
 
   def result
@@ -24,22 +22,20 @@ class InstitutionAccountController < ApplicationController
 
   def new
     @institution = InstitutionAccount.new
-    # @institution.build_institution_acc_billing_address
-  end
-
-  def create
-    @institution = InstitutionAccount.new(institution_params)
     #this is added inorder to build institution_acc_billing_address
     #we use this syntax because the association between institution account
     # and institution_acc_billing_address is has_one
     @institution.build_institution_acc_billing_address
+  end
 
+  def create
+    @institution = InstitutionAccount.new(institution_params)
+  
     if @institution.save!
       redirect_to institution_account_index_path
     else
       render 'new'
     end
-
   end
  
 
@@ -48,13 +44,9 @@ class InstitutionAccountController < ApplicationController
   def institution_params
     params.require(:institution_account).permit(:publisher,:institution_id, 
         :libary_name,:institution_name,:status,:display_name, 
-        { institution_acc_billing_address_attributes: [
+        institution_acc_billing_address_attributes: [ :id,
         :first_name, :last_name, :phone, :email, :address, 
         :address_line2,:address_line3, :city, :state, 
-        :postal_code, :comments, :institution_account_id]} ) 
-    # params = ActionController::Parameters.new({ institution_account: true })
-    # params.permit(:institution_account).tap do |whitelisted| 
-    #  whitelisted[:institution_acc_billing_address_attributes] = params[:institution_account][:institution_acc_billing_address] 
-    # end
+        :postal_code, :comments, :institution_account_id]) 
   end
 end
--- app/models/institution_account.rb
+++ app/models/institution_account.rb
@@ -1,5 +1,5 @@
 class InstitutionAccount < ApplicationRecord
 	include Filterable
-	has_one :institution_acc_billing_address
+	has_one :institution_acc_billing_address, dependent: :destroy
 	accepts_nested_attributes_for :institution_acc_billing_address
 end
