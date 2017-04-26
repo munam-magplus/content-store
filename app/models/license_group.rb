729e52643cb4746fa435b86616ce65add47545af
Kirti <kirtitrikha09@gmail.com>
2017-04-26 09:39:39 +0530

    Create Licenses

--- app/models/license_group.rb
+++ app/models/license_group.rb
@@ -1,4 +1,8 @@
 class LicenseGroup < ApplicationRecord
 	include Filterable
 	has_many :licenses, dependent: :destroy
+	# Validations
+  	#-----------------------------------------------------------------------------
+	validates_presence_of :license_group_name
+  	#-----------------------------------------------------------------------------
 end 
