5c458af1129975d4015955a8d4faddadde9a3c7a
shreeharshini <mamidalaharshini@gmail.com>
2017-04-28 13:09:08 +0530

    UI changes of institution account

--- app/models/institution_account.rb
+++ app/models/institution_account.rb
@@ -1,9 +1,9 @@
 class InstitutionAccount < ApplicationRecord
 	include Filterable
-	# belongs_to :institution_admin_user_acc
 	has_many :library_users
 	has_one :institution_acc_billing_address, dependent: :destroy
 	accepts_nested_attributes_for :institution_acc_billing_address
+	
 	#used for uploading image
 	has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
