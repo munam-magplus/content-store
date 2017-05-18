21895f6486c25195119f54589be2cd9f39f95873
Kirti <kirtitrikha09@gmail.com>
2017-05-18 10:00:58 +0530

    Code Updation Minor changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-	def publisher_name(pub)
-		@publisher = Publisher.find_by_id(pub)
-		@publisher = @publisher.publisher_name
+	def publisher_name(publisher_id)
+		publisher = Publisher.find_by_id(publisher_id)
+		@publisher = publisher.publisher_name
 	end
 	def campaign_name(campaign_id)
-		@campaign = Campaign.find_by_id(campaign_id)
-		@campaign = @campaign.campaign_name
+		campaign = Campaign.find_by_id(campaign_id)
+		@campaign = campaign.campaign_name
 	end
 	def publisher_names
 		@publishers = Publisher.order('publisher_name').all
