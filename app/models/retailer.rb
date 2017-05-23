52ef2046463079b02ceed7cc12311c7d43a47c9b
shreeharshini <mamidalaharshini@gmail.com>
2017-05-23 11:20:55 +0530

    changes in retailers

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,7 +1,7 @@
 class Retailer < ApplicationRecord
 	include Filterable #search module
 	# validating fields
-	validates :retailer_code, presence: true
+	validates :retailer_code, presence: true, uniqueness: true
 	validates :retailer_name, presence: true
 	validates :publisher_id, presence: true
 	validates :low_ip, presence: true
