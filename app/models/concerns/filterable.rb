8996d13dd759740d23e62d2849fd6daa930a15ef
Kirti <kirtitrikha09@gmail.com>
2017-06-23 14:33:14 +0530

    Advance Search

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -13,14 +13,21 @@ module Filterable
         if key == "to"
           cond << " AND created_at <=?" if value.present?
         end
-        if key != "from" && key != "to"
+        if key == "publication_date"
+          cond << " AND extract(year from publication_date) = ?" if value.present?
+        end
+        if key != "from" && key != "to" && key != "publication_date"
           # if key's value is present only then key will be added to cond
           cond << " AND #{key} LIKE ?" if value.present?
         end
       end
       filtering_params.each do |key, value|
         # if key's value is present only then value will be added to cond
-        cond << ",""%#{value}%" if value.present? 
+        if key == "publication_date"
+          cond << ",""#{value}" if value.present? 
+        else
+          cond << ",""%#{value}%" if value.present?
+        end 
       end
       # to split string into conditions
       conditions = cond.split(',')
