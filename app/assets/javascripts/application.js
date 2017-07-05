98e47866044a397a8c756750f4acfc63e0499155
aiyazmagplus <aiyaz@magplus.com>
2017-07-05 20:04:18 +0530

    UI changes for nyresearch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
+
 //=require jquery
 //=require jquery_ujs
+//= require bootstrap
 //= require jquery.remotipart
 //= require ckeditor/init
 //= require_tree .
