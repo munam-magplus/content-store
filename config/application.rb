7ac7cd5e83ac8ec072ee616d3099f46f4a7399ba
Arvind Tiwari <arvind@magplus.com>
2017-12-04 14:32:48 +0530

    add ip in application file

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module ContentStore
          '500' => 'application'
         },
     }
+    config.web_console.whitelisted_ips = '121.244.129.246'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
