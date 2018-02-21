67a18c5b4c666c35f327cf8a4aaa6998c4261caa
Kirti09 <kirtitrikha09@gmail.com>
2018-02-21 12:48:48 +0530

    Book's contributor edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = BooksPrimaryContentInformation.find(params[:id])
-    #@book_contributor = BooksContributor.find_by_books_primary_content_information_id(params[:id])
-    @book_contributor = BooksContributor.new
+    @book_contributor = BooksContributor.find_by_books_primary_content_information_id(params[:id])
     @book_content_pricing = BooksContentPricing.new
     @book_content_access_rule  = BooksContentAccessRule.new
     render 'new'
