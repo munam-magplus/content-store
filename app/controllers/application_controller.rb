c293f8997c8cd6e240437eaedc2c327d8a9585d9
Arvind Tiwari <arvind.tiwari796@gmail.com>
2018-03-05 10:52:45 +0530

    Add favicon icon for wtbooks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_action :set_publisher_name
   before_action :configure_permitted_parameters, if: :devise_controller?
   include SessionsHelper
  
   private
+
+  def set_publisher_name
+    publisher_domain = request.host
+    @publisher_name = publisher_domain.split('.')[1]
+  end
 
   def after_sign_in_path_for(resource)
     dashboard_content_conversations_path
