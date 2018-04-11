d376b4a8d30718427365797665a09439e2cb0aa9
Arvind Tiwari <arvind.tiwari796@gmail.com>
2018-04-11 12:11:39 +0530

    add commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :set_publisher_name
-  before_action :set_publisher_theme
+  #before_action :set_publisher_theme
   before_action :configure_permitted_parameters, if: :devise_controller?
   include SessionsHelper
  
@@ -20,11 +20,12 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
-  def set_publisher_theme
-    unless request.domain == "wtbooks.com"
-      @publisher_logo =  Publisher.where(domain_name: request.domain).last.publisher_logo_file_name
-    end
-  end
+  # def set_publisher_theme
+  #   byebug
+  #   unless request.domain == "wtbooks.com" 
+  #     @publisher_logo =  Publisher.where(domain_name: request.domain).last.publisher_logo_file_name
+  #   end
+  # end
 
   protected
 
