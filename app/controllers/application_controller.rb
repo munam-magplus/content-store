7ea0f3e070906dc01001d5ba0948aa6980ff5892
Arvind Tiwari <arvind.tiwari796@gmail.com>
2018-04-11 18:10:50 +0530

    add static link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,6 @@ class ApplicationController < ActionController::Base
   private
 
   def set_publisher_name
-    if request.host == "http://callistoreference.com"
-      @publisher_domain = publisher_domain.split('//')[1].split('.')[0]
-      @publisher_logo = Publisher.where(contact_first_name: publisher_domain).last.publisher_logo_file_name
-    end
-
     publisher_domain = request.host
     @publisher_name = publisher_domain.split('.')[1]
   end
